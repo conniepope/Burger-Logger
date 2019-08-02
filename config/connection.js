1b030081d97d14ceafc26a2fbc5431dede991d63
conniepope <conniepope77@gmail.com>
2019-08-02 10:58:52 -0400

    fixed syntax error to see if helps with heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -12,11 +12,12 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-    host: "localhost",
+    host: "localhost", 
     user: "root",
     password: keys.password,
     database: "burgers_db"
 });
+}
 
 connection.connect(function(err){
     if (err) throw err;
